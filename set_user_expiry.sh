4d01d5bb252bdc4c8735761cbbbf3cca42298446
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-22 18:31:50 +0000

    set date of expiry

---
+++ set_user_expiry.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <username> <expiry_date>"
+    echo "Expiry date format: YYY-MM-DD"
+    exit 1
+fi
+
+username=$1
+expiry_date=$2
+
+usermod -e $expiry_date $username
+echo "Expiration date for user $username set to $expiry_date"
