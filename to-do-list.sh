03fc558b7e6c224921d96b2e90b2df8a531a9d37
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-01-28 04:56:12 +0000

    task_2

--- to-do-list.sh
+++ to-do-list.sh
@@ -3,3 +3,18 @@
 # To-Do List Manager
 
 # Usage: ./todo_manager.sh [add|remove|list][task_number | task_description]
+TODO_FILE="todo_list.txt"
+
+if [ ! -f "$TODO_FILE"]; then
+    touch "$TODO_FILE"
+fi 
+
+case $1 in 
+    add)
+        if [ -z "$2" ]; then
+            echo "Please provide a task description."
+            exit
+        fi 
+        echo "$2" >> "$TODO_FILE"
+        echo "Task added successfully."
+        
